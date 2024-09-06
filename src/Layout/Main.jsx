e6ea844d4db121e51c09fe068aa24c328c21b7fa
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-06 22:01:27 +0600

    starting to design the login page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
 
-
 const Main = () => {
-    return (
-        <div className="">
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const location = useLocation();
+  const noHeaderFooter =
+    location.pathname.includes("login") || location.pathname.includes("signUp");
+
+  return (
+    <div className="">
+      {noHeaderFooter || <NavBar></NavBar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || <Footer></Footer>}
+    </div>
+  );
 };
 
 export default Main;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import ALLCourses from "../Pages/AllCourses/ALLCourses";
+import Login from "../Pages/Authentication/Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -13,9 +14,13 @@ export const router = createBrowserRouter([
             element: <Home></Home>
         },
         {
+            path:'/login',
+            element: <Login></Login>
+        },
+        {
           path: '/allCourses',
           element: <ALLCourses></ALLCourses>
-        }
+        },
     ]
   },
 ]);
