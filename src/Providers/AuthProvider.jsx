25934874bc9980400270ea3b133d73cac5138f93
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-08 21:40:31 +0600

    firebase setup done

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,10 @@
 import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
