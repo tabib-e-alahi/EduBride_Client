0fbd26a3e43f9071cb583c8fe061e103efc906a1
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-08 20:50:00 +0600

    footer 2 menu changed

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Education Services</a>
+          <a className="link link-hover">Education Consultant</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
         </nav>
