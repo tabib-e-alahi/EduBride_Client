b432c6139f362350d16e79b73d382a52555fa4e7
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-09 00:02:38 +0600

    react hook form added in sign up page

--- src/Pages/Authentication/SignUp/SignUp.jsx
+++ src/Pages/Authentication/SignUp/SignUp.jsx
@@ -1,11 +1,19 @@
 import SocialLogin from "../SocialLogin/SocialLogin";
-import login from '../../../assets/login.png'
+import login from "../../../assets/login.png";
 import { Link } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-    return (
-        <div className="h-screen flex items-center bg_custom">
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="h-screen flex items-center bg_custom">
       <div className="grid grid-cols-2 login_form_glass  max-w-6xl mx-auto h-5/6  shadow-lg rounded-sm shadow-slate-600">
         <div className="flex justify-center items-center ">
           <img className="w-2/3 mx-auto" src={login} alt="" />
@@ -17,23 +25,36 @@ const SignUp = () => {
           {/* social log in  */}
 
           <div className="rounded-sm">
-            <form  className="w-2/3 mx-auto">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 mx-auto">
               <div className="flex flex-col space-y-8">
                 <div className="flex flex-col gap-1">
                   <input
                     type="text"
                     name="name"
-                    className="p-3 rounded-md form-input placeholder:text-gray-600"
-                    placeholder="Full Name"
+                    className={`p-3 rounded-md form-input placeholder:text-gray-600 ${
+                      errors.name
+                        ? "error-input-border  shadow-inner shadow-red-300"
+                        : " input-border"
+                    }`}
+                    placeholder={`${
+                      errors.name ? "Please enter your full name" : "Full name"
+                    }`}
+                    {...register("name", { required: true })}
                   />
                 </div>
                 <div className="flex flex-col gap-1">
-                  
                   <input
                     type="email"
                     name="email"
-                    className=" p-3 rounded-lg form-input placeholder:text-gray-600"
-                    placeholder="Enter Email"
+                    className={`p-3 rounded-md form-input placeholder:text-gray-600 ${
+                      errors.email
+                        ? "error-input-border shadow-inner  shadow-red-300"
+                        : " input-border"
+                    }`}
+                    placeholder={`${
+                      errors.email ? "Please enter valid email" : "Enter email"
+                    }`}
+                    {...register("email", { required: true })}
                   />
                 </div>
 
@@ -41,8 +62,17 @@ const SignUp = () => {
                   <input
                     type="password"
                     name="password"
-                    className="p-3 rounded-lg form-input  placeholder:text-gray-600"
-                    placeholder="Enter Password"
+                    className={`p-3 rounded-md form-input placeholder:text-gray-600 ${
+                      errors.password
+                        ? "error-input-border shadow-inner  shadow-red-300"
+                        : " input-border"
+                    }`}
+                    placeholder={`${
+                      errors.password
+                        ? "Please enter valid password"
+                        : "Enter password"
+                    }`}
+                    {...register("password", { required: true })}
                   />
                 </div>
 
@@ -51,7 +81,6 @@ const SignUp = () => {
                     SignUp
                   </button>
                 </div>
-
               </div>
             </form>
 
@@ -66,7 +95,7 @@ const SignUp = () => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default SignUp;
