9b5343fe54a344a0358d2779cd02f46b0569470e
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-10 12:56:22 +0600

    login funtion addeed

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Login.css";
 import login from "../../../assets/login.png";
 import SocialLogin from "../SocialLogin/SocialLogin";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../Providers/AuthProvider";
+import { useForm } from "react-hook-form";
+import { BiSolidError } from "react-icons/bi";
 
 const Login = () => {
-  const handleLogin = () => {};
+  const [credintialErrors, setCredintialErrors] = useState(null);
+  const { signIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location?.state?.from?.pathname || "/";
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    signIn(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        navigate(from, { replace: true });
+      })
+      .catch((errors) => setCredintialErrors(errors.message));
+  };
 
   return (
     <div className="h-screen flex items-center bg_custom">
@@ -12,21 +38,36 @@ const Login = () => {
         <div className="flex justify-center items-center ">
           <img className="w-2/3 mx-auto" src={login} alt="" />
         </div>
-        <div className="font-sans w-full  my-auto ">
+        <div className="noto-sans-font w-full  my-auto ">
           <h1 className="text-center  text-4xl font-bold mb-6 text-[#172734]">
             Log in
           </h1>
-          {/* social log in  */}
+
+          {credintialErrors && (
+            <div className="w-2/3 mx-auto mb-6 p-4 h-fit  bg-[#fef0ee] text-left flex gap-4 justify-center items-center">
+              <BiSolidError className="text-red-600 text-4xl" />
+              <p className="text-sm font-bold text-red-600">
+                The email address or password you entered were invalid.
+              </p>
+            </div>
+          )}
 
           <div className="rounded-sm">
-            <form onSubmit={handleLogin} className="w-2/3 mx-auto">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 mx-auto">
               <div className="flex flex-col space-y-8">
                 <div className="flex flex-col gap-1">
                   <input
                     type="email"
                     name="email"
-                    className=" p-3 rounded-lg form-input placeholder:text-gray-600"
-                    placeholder="Enter Email"
+                    className={`p-3 rounded-md form-input placeholder:text-gray-600 ${
+                      errors.email
+                        ? "error-input-border shadow-inner  shadow-red-300"
+                        : " input-border"
+                    }`}
+                    placeholder={`${
+                      errors.email ? "Please enter valid email" : "Enter email"
+                    }`}
+                    {...register("email", { required: true })}
                   />
                 </div>
 
@@ -34,8 +75,19 @@ const Login = () => {
                   <input
                     type="password"
                     name="password"
-                    className="p-3 rounded-lg form-input  placeholder:text-gray-600"
-                    placeholder="Enter Password"
+                    className={`p-3 rounded-md form-input placeholder:text-gray-600 ${
+                      errors.password
+                        ? "error-input-border shadow-inner  shadow-red-300"
+                        : "input-border"
+                    }`}
+                    placeholder={`${
+                      errors.password
+                        ? "Please enter valid password"
+                        : "Enter password"
+                    }`}
+                    {...register("password", {
+                      required: true,
+                    })}
                   />
                 </div>
 
--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return (
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-400 dark:border-violet-600"></div>
+    );
+  }
+  if (user) {
+    return children;
+  }
 
-const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
-    if(user){
-        return children;
-    }
-
-    return <Navigate to='/login'></Navigate>
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
