d1017e3d803731dfecb9200152a5947ce2f828c7
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-13 11:41:45 +0600

    banner pauseOnmouseover added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 import Banner1 from "./Banner1";
 import Banner2 from "./Banner2";
 import Banner3 from "./Banner3";
@@ -14,7 +14,16 @@ import Banner3 from "./Banner3";
 const Banner = () => {
   return (
     <div className="mb-28 pt-20 rounded-xl max-w-7xl mx-auto ">
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper  h-fit">
+      <Swiper
+        pagination={true}
+        autoplay={{
+          delay: 1500,
+          // disableOnInteraction: true,
+          pauseOnMouseEnter: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper  h-fit rounded-xl"
+      >
         <SwiperSlide>
           <Banner1></Banner1>
         </SwiperSlide>
