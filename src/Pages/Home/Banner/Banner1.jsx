358faa4b3708a2b48c9997a4d4035297f28c9f37
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-15 22:06:01 +0600

    banner fadein out animationa nd button style modified

--- src/Pages/Home/Banner/Banner1.jsx
+++ src/Pages/Home/Banner/Banner1.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
-import banner_image from '../../../assets/banner_image.png'
+import { Link } from "react-router-dom";
+import banner_image from "../../../assets/banner_image.png";
 // import banner_bg from '../../../assets/banner_bg.png'
-import 'animate.css';
+import "animate.css";
 
 const Banner1 = () => {
   return (
@@ -9,31 +9,26 @@ const Banner1 = () => {
       <section className=" lg:h-fit rounded-md w-11/12 mx-auto">
         <div className="grid  py-8 lg:grid-cols-2 lg:pt-4">
           <div className="mr-auto place-self-center justify-center  w-11/12 ">
-            <h1 className="animate__animated animate__lightSpeedInLeft max-w-2xl mb-4 roboto-mono-font font-bold leading-none tracking-tight md:text-5xl line_class  dark:text-black">
-              Experience <br/>Personalized<br />
+            <h1 className="animate__animated animate__fadeInLeft animate__slow max-w-2xl mb-4 roboto-mono-font font-bold leading-none tracking-tight md:text-5xl line_class  dark:text-black">
+              Experience <br />
+              Personalized
+              <br />
               learning with our <br />
               private course.
             </h1>
-            <p className="max-w-2xl mb-6 font-semibold  text-gray-500 lg:mb-8  dark:text-gray-600">
-            Discover tailored education with our private course, offering a personalized learning experience that caters to your unique needs and goals.
-              .
+            <p className="animate__animated animate__fadeInRight animate__slow max-w-2xl mb-6 font-semibold  text-gray-500 lg:mb-8  dark:text-gray-600">
+              Discover tailored education with our private course, offering a
+              personalized learning experience that caters to your unique needs
+              and goals. .
             </p>
-            <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              <Link
-                className="inline-flex bg-white font-bold text-[#26507F] items-center justify-center w-full px-5 py-3 text-sm  text-center hover:text-gray-100 hover:bg-[#ED9A15] rounded-lg sm:w-auto"
-              >
-                
+            <div className="animate__animated  animate__fadeInUp animate__slower space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+              <Link className="flex-none rounded-sm bg-indigo-500 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                 View Courses
               </Link>
-             
             </div>
           </div>
-          <div className="hidden lg:mt-0  lg:flex justify-end">
-            <img
-            className='h-fit w-full'
-              src={banner_image}
-              alt="hero image"
-            />
+          <div className="hidden lg:mt-0  lg:flex justify-end animate__animated animate__fadeInRight animate__slow">
+            <img className="h-fit w-full" src={banner_image} alt="hero image" />
           </div>
         </div>
       </section>
