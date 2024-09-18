9eae4ae5008417f957ae5016d93f605f40a90149
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-18 23:12:34 +0600

    html project card style added on these project cards

--- src/Pages/Home/PopularCourse/SinglePopularCourses.jsx
+++ src/Pages/Home/PopularCourse/SinglePopularCourses.jsx
@@ -24,25 +24,48 @@ const SinglePopularCourses = ({ p }) => {
   // };
 
   return (
-    <div className="max-w-sm h-[380px] shadow-sm rounded-lg  flex flex-col justify-between noto-sans-font">
-      <img className="rounded-t-xl" src={image} alt="" />
-      <div className="bg-[#F9F9FA] py-3 px-2 text-sm font-semibold border-x border-gray-300 flex justify-between ">
-        <span className="rounded-3xl ">Duration: {duration_hours}hr</span>
-        <div className="flex gap-1">
-          <Rating style={{ maxWidth: 90 }} value={ratings} readOnly />
-          <span className="font-bold">({ratings})</span>
+    <div className="max-w-sm h-full p-4 border bg-white shadow-sm rounded-3xl font-sans  flex flex-col justify-between noto-sans-font">
+      <div>
+        <div className="flex gap-2 justify-start items-center mb-4 pl-6">
+          <img
+            className="rounded-full size-6 outline outline-indigo-400 "
+            src={image}
+            alt
+          />
+          <p className="font-medium font-serif">Yet to be announced</p>
         </div>
-      </div>
-      <div className="p-5 flex-grow border-x border-gray-300 bg-white">
-        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
-          {title}
-        </h5>
-        <p className="text-sm">{author}</p>
-      </div>
-      <div className="bg-[#F9F9FA] py-2 flex justify-center border-x border-gray-300 border-b rounded-b-xl">
-        <button className="flex-none rounded-sm bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-          See Details <FaLongArrowAltRight className="inline-flex" />
-        </button>
+        <img
+          className="rounded-lg mb-2 w-[353px] h-[200px]"
+          src={image}
+          alt
+        />
+        <div className=" flex-grow p-6  bg-white">
+          <div className="badge mb-3 bg-[#EDF8F9] text-[#17A2B8] rounded-none py-3 text-xs px-1.5 ">
+            {author}
+          </div>
+          <div className="flex justify-start  gap-x-5">
+            <div className="text-sm font-semibold text-[#94928e]">
+              {lessons}
+            </div>
+            <div className="text-sm font-semibold text-[#94928e]">
+              {enrolled_students} enrolled
+            </div>
+          </div>
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+            {title}
+          </h5>
+          <span className="font-medium">
+            Duration: {duration_hours}hr
+          </span>
+        </div>
+        <div className="border-t px-2 w-11/12 mx-auto   py-2 flex justify-between items-center  rounded-b-xl">
+          <button className="font-semibold text-indigo-700 ">
+            See Details
+          </button>
+          <span className="font-bold text-indigo-600 text-xl">
+            ${price > 0 ? '$'+ price : "Free"}
+          </span>
+        </div>
       </div>
     </div>
   );
