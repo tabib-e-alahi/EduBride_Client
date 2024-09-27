bcab4406d2360297bf679974440cc55eda440e99
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-27 22:58:20 +0600

    tried to implent carousel but failded

--- src/Pages/Home/Testimonials/FeedBackCards/FeedBackCard.jsx
+++ src/Pages/Home/Testimonials/FeedBackCards/FeedBackCard.jsx
@@ -1,39 +1,31 @@
-import { motion } from "framer-motion";
-
-const FeedBackCard = ({ fb, handleFeedBackCardClick }) => {
+const FeedBackCard = ({ fb }) => {
   return (
-    <motion.div
-      layoutId={fb.id}
-      onClick={() => handleFeedBackCardClick(fb.id)}
-      className="h-60 lg:w-72 border rounded-sm shadow-sm flex justify-center items-center"
-    >
-      <motion.section className="  ">
-        <motion.div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-12 lg:px-1">
-          <motion.figure className="max-w-screen-md mx-auto">
-            <motion.blockquote>
-              <motion.p className="text-sm font-medium text-gray-900 md:text-lg ">
+    <div className="carousel__cell h-60 lg:w-72 border rounded-sm shadow-sm flex justify-center items-center">
+      <section className="  ">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-12 lg:px-1">
+          <figure className="max-w-screen-md mx-auto">
+            <blockquote>
+              <p className="text-sm font-medium text-gray-900 md:text-lg ">
                 "{fb.feedback}"
-              </motion.p>
-            </motion.blockquote>
-            <motion.figcaption className="flex items-center justify-center mt-6 space-x-3">
+              </p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
               <img
                 className="w-6 h-6 rounded-full"
                 src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
                 alt="profile picture"
               />
-              <motion.div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                <motion.div className="pr-3 font-medium text-gray-900 ">
-                  {fb.name}
-                </motion.div>
-                <motion.div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
+              <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                <div className="pr-3 font-medium text-gray-900 ">{fb.name}</div>
+                <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
                   {fb.course}
-                </motion.div>
-              </motion.div>
-            </motion.figcaption>
-          </motion.figure>
-        </motion.div>
-      </motion.section>
-    </motion.div>
+                </div>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+    </div>
   );
 };
 
--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import FeedBackCard from "./FeedBackCards/FeedBackCard";
-import ExpandedFeedBackCard from "./FeedBackCards/ExpandedFeedBackCard";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
+const carousel = (slider) => {
+  const z = 300;
+  function rotate() {
+    const deg = 360 * slider.track.details.progress;
+    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
+  }
+  slider.on("created", () => {
+    const deg = 360 / slider.slides.length;
+    slider.slides.forEach((element, idx) => {
+      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`;
+    });
+    rotate();
+  });
+  slider.on("detailsChanged", rotate);
+};
 
 const Testimonials = () => {
   const [feedBacks, setFeedBacks] = useState([]);
-  const [selectedId, setSelectedId] = useState(null);
 
   useEffect(() => {
     fetch("./feedback.json")
@@ -13,23 +28,34 @@ const Testimonials = () => {
       .then((data) => setFeedBacks(data));
   }, []);
 
-  const handleFeedBackCardClick = (id) => {
-    setSelectedId(id);
-  };
-
-  const handleFeedBackCardClose = () => {
-    setSelectedId(null);
-  };
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      selector: ".carousel__cell",
+      renderMode: "custom",
+      mode: "free-snap",
+    },
+    [carousel]
+  );
 
   return (
-    <div className="grid grid-cols-1 w-full lg:grid-cols-4 gap-4 lg:gap-10 lg:mb-10">
-      {feedBacks.map((fb) => (
-        <FeedBackCard
-          key={fb.id}
-          fb={fb}
-          handleFeedBackCardClick={handleFeedBackCardClick}
-        ></FeedBackCard>
-      ))}
+    <div className="wrapper ">
+      <div className="scene">
+        <div className="carousel keen-slider" ref={sliderRef}>
+          {/* <div className="carousel__cell number-slide1 ">1</div>
+         <div className="carousel__cell number-slide2">2</div>
+         <div className="carousel__cell number-slide3">3</div>
+         <div className="carousel__cell number-slide4">4</div>
+         <div className="carousel__cell number-slide5">5</div>
+         <div className="carousel__cell number-slide6">6</div> */}
+          {feedBacks.map((fb) => (
+            <FeedBackCard
+              key={fb.id}
+              fb={fb}
+            ></FeedBackCard>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
