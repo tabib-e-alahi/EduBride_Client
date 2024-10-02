5a8abdaada30933e194c983d447c93addff2a942
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-03 00:05:43 +0600

    course details page updated

--- src/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/CourseDetails/CourseDetails.jsx
@@ -27,10 +27,40 @@ const CourseDetails = () => {
 
   return (
     <div className="max-w-7xl mx-auto">
-     <div className="mt-24">
-     <AllCourseBanner pageTitle={pageTitle}></AllCourseBanner>
-     </div>
-      <h1 className="text-3xl text-center mt-16">{course?.course_title}</h1>
+      <div className="mt-24">
+        <AllCourseBanner pageTitle={pageTitle}></AllCourseBanner>
+      </div>
+      <section className="grid grid-cols-4">
+        <div className="col-span-2 space-y-10">
+          <img
+            className="rounded-tl-box rounded-br-xl"
+            src={course?.cover_image}
+            alt=""
+          />
+          <p>{course?.about_this_course}</p>
+        </div>
+        <div className="col-span-2">
+          <div className="card rounded-xl bg-base-100 w-96 ml-auto shadow-xl">
+            <figure>
+              <img
+                src={course?.cover_image}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
