ada7a1063cade0bb240a897295d96d43bf82438d
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-11 23:56:36 +0600

    couse details page modified

--- src/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/CourseDetails/CourseDetails.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
 import AllCourseBanner from "../AllCourses/AllCourseBanner";
+import "./CourseDetails.css";
+import PageLoader from "../Shared/PageLoader/PageLoader";
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -38,44 +40,54 @@ const CourseDetails = () => {
   const pageTitle = (
     <>
       <h1 className="text-white font-bold text-center text-4xl xl:text-5xl">
-        {course_title}
+        {category}
       </h1>
     </>
   );
 
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="mt-24">
-        <AllCourseBanner pageTitle={pageTitle}></AllCourseBanner>
-      </div>
-      <section className="grid grid-cols-4">
-        <div className="col-span-2 space-y-10">
-          <img
-            className="rounded-tl-box rounded-br-xl"
-            src={cover_image}
-            alt="course cover image"
-          />
-          <p className="roboto-mono-font text-justify font-medium">{about_this_course}</p>
-        </div>
-        <div className="col-span-2">
-          <div className="card rounded-xl bg-base-100 w-96 ml-auto shadow-xl">
-            <figure>
-              <img src={cover_image} alt="course cover image" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {course_title}
-              </h2>
-              <p></p>
-              <div className="card-actions justify-end">
-                <div className="py-2 px-5 rounded-lg bg-[#4A6DB0] text-white">{price}</div>
-                <div className="bg-[#4A6DB0] text-white">Enroll Now</div>
+    <>
+      {isLoading ? (
+        <PageLoader></PageLoader>
+      ) : (
+        <div className="max-w-7xl mx-auto">
+          <div className="mt-24">
+            <AllCourseBanner pageTitle={pageTitle}></AllCourseBanner>
+          </div>
+          <section className="grid grid-cols-3">
+            <div className="col-span-2 space-y-10">
+              <h1 className="text-4xl font-bold font-serif">{course_title}</h1>
+              <p className="roboto-mono-font text-justify font-medium">
+                {about_this_course}
+              </p>
+              <img
+                className="rounded-tl-box rounded-br-xl"
+                src={cover_image}
+                alt="course cover image"
+              />
+              
+            </div>
+            <div className="1">
+              <div className="card rounded-xl bg-base-100 w-96 ml-auto shadow-xl">
+                <figure>
+                  <img src={cover_image} alt="course cover image" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{course_title}</h2>
+                  <p></p>
+                  <div className="card-actions justify-between">
+                    <div className="text-[#4A6DB0] text-xl font-semibold">
+                      ${price}
+                    </div>
+                    <button className="enroll_btn_class">Enroll</button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
-      </section>
-    </div>
+      )}
+    </>
   );
 };
 
